7836eab5b2fc35c9294137d91be3a86fcd5e31a5
Bob Ferris <zazi@smiy.org>
2011-07-22 13:52:15 +0200

    - [ADDED] PureVolumeTranslator - [GENERATED] fresh jar from the sources

---
+++ linkedbrainz-d2rs-translators/src/main/java/linkedbrainz/d2rs/translator/PureVolumeTranslator.java
@@ -0,0 +1,18 @@
+package linkedbrainz.d2rs.translator;
+
+import linkedbrainz.d2rs.translator.util.BaseUriCleanUpTranslator;
+
+/**
+ * 
+ * @author zazi
+ * 
+ */
+public class PureVolumeTranslator extends BaseUriCleanUpTranslator
+{
+	public static final String ORIGINAL_BASE_URI = "http://purevolume.com/";
+
+	public PureVolumeTranslator()
+	{
+		super(ORIGINAL_BASE_URI);
+	}
+}
